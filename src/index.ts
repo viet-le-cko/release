871ee8d76cc60bbeb0b111ea0b0da89b54e7cb1c
Viet <viet.le@checkout.com>
2020-04-08 16:02:40 +0100

    Get and map tickets

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import axios from 'axios';
+
+const getCommitsBetweenReleases = async (start: string, end: string) => {
+  const response = await axios.get(
+    `https://api.github.com/repos/viet-le-cko/release-test/compare/${start}...${end}`
+  );
+  return response.data.commits;
 };
+
+(async () => {
+  const commits = await getCommitsBetweenReleases(
+    'build-number-1111',
+    'build-number-5555'
+  );
+
+  const jiraTicketRegex = new RegExp(/(^PRISM-\d*)/);
+
+  console.log(
+    commits
+      .map(({ commit }: any) => {
+        return commit.message;
+      })
+      .filter((message: string) => {
+        return jiraTicketRegex.test(message);
+      })
+      .map((message: string) => {
+        const prismCommits = message.match(jiraTicketRegex);
+        if (prismCommits !== null) {
+          return prismCommits[0];
+        }
+        return message;
+      })
+  );
+
+  return null;
+})();
